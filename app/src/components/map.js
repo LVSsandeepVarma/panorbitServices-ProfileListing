981285bc2217f80c45e0f815159c655245c8c19a
LVSsandeepVarma <sandeeplakham@gmail.com>
2023-05-18 12:15:16 +0530

    added maps in profile section

--- app/src/components/map.js
+++ app/src/components/map.js
@@ -1,9 +1,30 @@
 import React from "react"
+import {MapContainer, TileLayer, Marker, Tooltip} from "react-leaflet"
+import { useSelector } from "react-redux";
+import { selectedContact } from "../features/profiles/profilesSlice";
 
-export default function Map(){
+
+const mapStyle = {
+    width: '100%',
+    height: '350px',
+    justifyContent: "center"
+  };
+
+export default function MapComponent(){
+    const contactDetails = useSelector(selectedContact);
+
     return(
         <div>
-            Map Component
-        </div>
+      <MapContainer style={mapStyle} center={[contactDetails[0]?.address?.geo?.lat, contactDetails[0]?.address?.geo?.lng]} zoom={1} scrollWheelZoom={true}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={[contactDetails[0]?.address?.geo?.lat, contactDetails[0]?.address?.geo?.lng]}>
+        <Tooltip>
+                latitude: {contactDetails[0]?.address?.geo?.lat} <br />
+                Longitude: {contactDetails[0]?.address?.geo?.lng} <br />
+              </Tooltip>
+        </Marker>
+      </MapContainer>
+    </div>
+            
     )
 }
--- app/src/components/profile.jsx
+++ app/src/components/profile.jsx
@@ -12,6 +12,7 @@ export default function Profile(){
                     <Header/>
                     <DetailedProfile/>
                 </div>
+                
             </div>
 
         </div>
