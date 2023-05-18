76cc66fcb13474cbf02f884dc015779532fbd5d1
LVSsandeepVarma <sandeeplakham@gmail.com>
2023-05-18 08:41:42 +0530

    profile component with inner deetails first cut

---
+++ app/src/components/detailedProfile.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Card from "react-bootstrap/Card"
+import { selectedContact } from "../features/profiles/profilesSlice";
+import { useSelector } from "react-redux";
+import { left } from "@popperjs/core";
+
+export default function DetailedProfile(){
+    const contactDetails = useSelector(selectedContact);
+
+    return(
+        <div className="detailComponent" style={{background:"none"}}>
+            <div>
+                <Card style={{width:"20rem", marginBottom:"10%", border:"none"}}>
+                    <Card.Body>
+                        <Card.Img className="rounded-circle" variant="top" src={contactDetails[0].profilepicture} alt="profile pic" />
+                        <Card.Title>{contactDetails[0]?.name}</Card.Title>
+                        <Card.Subtitle className="text-muted" >Username: <b className="details">{contactDetails[0]?.username}</b></Card.Subtitle >
+                        <Card.Subtitle className="text-muted" >Email: <b className="details">{contactDetails[0]?.email}</b></Card.Subtitle >
+                        <Card.Subtitle className="text-muted" >Phone: <b className="details">{contactDetails[0]?.phone}</b></Card.Subtitle >
+                        <Card.Subtitle className="text-muted" >Website: <Card.Link className="details" href={contactDetails[0]?.website}>{contactDetails[0]?.website}</Card.Link></Card.Subtitle >
+                        <hr></hr>
+                        <Card.Subtitle className="text-muted" >Company</Card.Subtitle >
+                        <Card.Subtitle className="text-muted" >Name: <b className="details">{contactDetails[0]?.name}</b></Card.Subtitle >
+                        <Card.Subtitle className="text-muted" >catchPhrase: <b className="details">{contactDetails[0]?.company?.catchPhrase}</b></Card.Subtitle >
+                        <Card.Subtitle className="text-muted" >bs: <b className="details">{contactDetails[0]?.company?.bs}</b></Card.Subtitle >
+
+
+                    </Card.Body>
+                </Card>
+            </div>
+            <div className="verticalDivider"></div>
+            <Card style={{width:"100%"}}>
+                <Card.Body>
+                    <Card.Title style={{textAlign: left}}>Address :</Card.Title>
+                <Card.Subtitle className="text-muted" >Street: <b className="details">{contactDetails[0]?.address?.street}</b></Card.Subtitle >
+                <Card.Subtitle className="text-muted" >Suite: <b className="details">{contactDetails[0]?.address?.suite}</b></Card.Subtitle >
+                <Card.Subtitle className="text-muted" >City: <b className="details">{contactDetails[0]?.address?.city}</b></Card.Subtitle >
+                <Card.Subtitle className="text-muted" >Zipcode: <b className="details">{contactDetails[0]?.address?.zipcode}</b></Card.Subtitle >
+
+                </Card.Body>
+            </Card>
+        </div>
+    )
+}
--- app/src/components/profile.jsx
+++ app/src/components/profile.jsx
@@ -1,9 +1,19 @@
 import React from "react"
+import Sidenavbar from "./navbars/sidenavbar"
+import Header from "./navbars/navbar"
+import DetailedProfile from "./detailedProfile"
 
 export default function Profile(){
     return(
         <div>
-            profile Component
+            <div className="sideNavWrapper">
+                <Sidenavbar/>
+                <div className="profileComponent">
+                    <Header/>
+                    <DetailedProfile/>
+                </div>
+            </div>
+
         </div>
     )
 }
